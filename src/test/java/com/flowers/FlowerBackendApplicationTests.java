267e7f9b63e97faee3994a49993749c9fc522acd
thirtsoft <thirtsoft@gmail.com>
2021-08-16 14:30:20 +0000

    first commit

---
+++ src/test/java/com/flowers/FlowerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
