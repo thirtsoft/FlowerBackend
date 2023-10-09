5bdfd32516a169790870cc7950ec3b9787d9d002
asamba <abdoulaye.samba@awaconsulting.com>
2023-10-09 22:49:16 +0000

    add zone price

---
+++ src/main/java/com/flowers/utils/BillingPrice.java
@@ -0,0 +1,12 @@
+package com.flowers.utils;
+
+public interface BillingPrice {
+
+    public static double ZONE01  = 1000;
+    public static double ZONE02 = 1500;
+    public static double ZONE03  = 2000;
+    public static double ZONE04 = 2500;
+    public static double ZONE05 = 3000;
+    public static double ZONE06 = 4000;
+    public static double AUTRE_ZONE = 5000;
+}
